54128a3a85f446c914b79321efd4d89f7d1f3421
asei0003 <asei0003@student.monash.edu>
2020-10-21 14:09:51 +1100

    updated argument name in test case

--- tests/testthat/test-generate_value_box.R
+++ tests/testthat/test-generate_value_box.R
@@ -1,6 +1,6 @@
 values_vector_input <- c(37,42,75,29,46,85)
 text_input <- "Confirmed Cases"
-ui_valuebox <- generate_value_box(counts_vector=values_vector, text=text_input, icon="head-side-mask", color="green")
+ui_valuebox <- generate_value_box(counts_vector=values_vector_input, text=text_input, icon="head-side-mask", color="green")
 
 test_that("expect sum of vector", {
   expect_equal(sum(values_vector_input),
